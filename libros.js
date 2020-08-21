e19d5c87f57409ddb6468ce8e0034166eaa5ba63
yume1212 <you@example.com>
2020-08-20 20:37:16 -0500

    array libros

---
+++ libros.js
@@ -0,0 +1,78 @@
+/*NERUDA*/
+
+libros: [
+{
+    id:1,
+    titulo: "Veinte poemas de amor y una canción desesperada",
+    genero: "poesia",
+    anio publicacion: 1924
+},
+
+{
+    id:2,
+    titulo:"Libro de las preguntas"
+    genero: "poesia"
+    anio publicacion: 1974
+},{
+    id:3,
+    titulo:"La Barcarola",
+    genero: "poesia",
+    anio publicacion: 1967
+},
+{
+    id:4,
+    titulo:"Cien sonetos de amor",
+    genero: "poesia",
+    anio publicacion: 1959
+},
+]
+/*MARQUEZ*/
+libros: [
+    {
+        id:5,
+        titulo: "Cien anios de soledad",
+        genero: "Drama y realismo mágico",
+        anio publicacion: 1967
+    },
+    
+    {
+        id:6,
+        titulo:"Libro de las preguntas",
+        genero: "poesia",
+        anio publicacion: 1974
+    },{
+        id:7,
+        titulo:"El amor en los tiempos del colera",
+        genero: "Novela romantica",
+        anio publicacion: 1985
+    },
+    {
+        id:8,
+        titulo:"Cronicas de una muerte anunciada",
+        genero: "Novela romantica",
+        anio publicacion: 1981
+    },
+    ]
+    /*BORGES*/
+
+    libros: [
+        {
+            id:9,
+            titulo: "El informe de Brodie",
+            genero: "Cuento",
+            anio publicacion: 1970
+        },
+        
+        {
+            id:10,
+            titulo:"Historia universal de la infamia",
+            genero: "Cuento",
+            anio publicacion: 1936
+        },{
+            id:11,
+            titulo:"La moneda de hierro",
+            genero: "Poesia",
+            anio publicacion: 1976
+        },
+        
+        ]
