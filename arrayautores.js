7a27abfa980efc91b809ab37d035216acbc728f5
JuliB23 <jabp23@gmail.com>
2020-08-20 20:26:41 -0500

    array de los autores

---
+++ arrayautores.js
@@ -0,0 +1,34 @@
+/*Autores*/
+[
+    id: 1,
+    nombre: "Pablo",
+    apellido: "Neruda",
+    fechadeNacimiento: "12/07/1904",
+
+]
+[
+    id: 2,
+    nombre: "Gabriel",
+    apellido: "García Márquez",
+    fechadeNacimiento: "06/03/1927",
+]
+[
+    id: 3,
+    nombre: "Jorge Luis",
+    apellido: "Borges",
+    fechadeNacimiento: "24/08/1899",
+
+]
+/*constructor
+class autor {
+    constructor (id, titulo, descripcion, año){
+      this.id = id;
+      this.titulo = titulo;
+      this.descripcion = descripcion;
+      this.año = año;
+      this.libro = [id, titulo, descripcion, año]
+    }
+  }
+  libro1 = new autor ("Jorge", "sfdff", "dfsfdf", 1900 );
+  
+  console.log(libro1.libro);/*
