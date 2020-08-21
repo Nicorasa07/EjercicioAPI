7f15a3882360343a5e6c9c488b631685ea574075
Maria Fernanda Garcia Pineda <maria.fernanda.garcia1@hp.com>
2020-08-20 20:47:42 -0500

    Libros

--- libros.js
+++ libros.js
@@ -12,35 +12,66 @@ app.listen(3000, () => {
 server.get("/autores/:id/libros", (req, res) => {
   const id = req.params.id;
 
-  const autor = autores.find((autor) => autor.id == id);
-  res.json(autor.libros);
+  if (isNaN(id)) {
+    res.status(404).json({ error: "Autor no válido" });
+  } else {
+    const autor = autores.find((autor) => autor.id == id);
+    res.status(200).json(autor.libros);
+  }
 });
 
 server.post("/autores/:id/libros", (req, res) => {
   const { body } = req;
   const id = req.params.id;
 
-  const autor = autores.find((autor) => autor.id == id);
-  const libros = autor.libros;
-  libros.push(body);
+  if (isNaN(id)) {
+    res.status(404).json({ error: "Autor no válido" });
+  } else {
+    const autor = autores.find((autor) => autor.id == id);
+    const libros = autor.libros;
+    libros.push(body);
 
-  res.status(200).json("Agregado");
+    res.status(201).json("Agregado");
+  }
 });
 
 server.get("/autores/:id/libros/:idLibro", (req, res) => {
   const id = req.params.id;
   const idLibro = req.params.idLibro;
 
-  const autor = autores.find((autor) => autor.id == id);
-  const libros = autor.libros;
-  const libro = libros.find((libro) => libro.id == idLibro);
+  if (isNaN(id)) {
+    res.status(404).json({ error: "Autor no válido" });
+  } else if (isNaN(idLibro)) {
+    res.status(404).json({ error: "Autor no válido" });
+  } else {
+    const autor = autores.find((autor) => autor.id == id);
 
-  res.json(autor.libro);
+    const libros = autor.libros;
+    const libro = libros.find((libro) => libro.id == idLibro);
+
+    res.status(200).json(autor.libro);
+  }
 });
 
 server.put("/autores/:id/libros/:idLibro", (req, res) => {
   const id = req.params.id;
   const idLibro = req.params.idLibro;
+
+  if (isNaN(id) || isNaN(idLibro)) {
+    res.status(400).json({ error: "Solicitud no válida" });
+  } else {
+    const autor = autores.find((autor) => autor.id == id);
+    const libros = autor.libros;
+    const libro = libros.find((libro) => libro.id == idLibro);
+
+    if (libro) {
+      const i = libros.indexOf(libro);
+      libros[i] = libro;
+      res.send("Autor Actualizado");
+    } else {
+      res.status(404).send("Id no encontrado");
+    }
+  }
 });
 
 server.delete("/autores/:id/libros/:idLibro", (req, res) => {
